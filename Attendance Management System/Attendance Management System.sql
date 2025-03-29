2f5457b38504a99896150d49675765a06dbe8105
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2025-03-29 20:55:03 +0530

    Add files via upload
    Attendance System Project

---
+++ Attendance Management System/Attendance Management System.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Attendance_Management_System;
+
+CREATE TABLE Ajay_Table
+(
+User_ID INT IDENTITY(1,1),
+User_Name VARCHAR(150) UNIQUE,
+User_Pass VARCHAR(150),
+User_Pho VARCHAR(15),
+User_CNIC VARCHAR(15) UNIQUE,
+User_DOB VARCHAR(10),
+User_Gender VARCHAR(6),
+User_Email VARCHAR(30),
+User_Role VARCHAR(5),
+User_Add VARCHAR(150),
+CONSTRAINT Ajay_Table_PK PRIMARY KEY (User_ID)
+);
+
+INSERT INTO Ajay_Table VALUES
+('user','123','+11 111 1111','11111-11111-1','01/01/2005','Male','user@gmail.com','User','India'),
+('admin','1234','+22 222 2222','22222-22222-2','02/02/2005','Male','admin@gmail.com','Admin','Canada');
