445542a2806bee4519f172051e9e298556425a35
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2025-03-30 11:17:40 +0530

    Update Attendance Management System.sql
    Table Name Updated

--- Attendance Management System/Attendance Management System.sql
+++ Attendance Management System/Attendance Management System.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Attendance_Management_System;
 
-CREATE TABLE Ajay_Table
+CREATE TABLE User_Table
 (
 User_ID INT IDENTITY(1,1),
 User_Name VARCHAR(150) UNIQUE,
@@ -12,9 +12,9 @@ User_Gender VARCHAR(6),
 User_Email VARCHAR(30),
 User_Role VARCHAR(5),
 User_Add VARCHAR(150),
-CONSTRAINT Ajay_Table_PK PRIMARY KEY (User_ID)
+CONSTRAINT User_Table_PK PRIMARY KEY (User_ID)
 );
 
-INSERT INTO Ajay_Table VALUES
+INSERT INTO User_Table VALUES
 ('user','123','+11 111 1111','11111-11111-1','01/01/2005','Male','user@gmail.com','User','India'),
 ('admin','1234','+22 222 2222','22222-22222-2','02/02/2005','Male','admin@gmail.com','Admin','Canada');
