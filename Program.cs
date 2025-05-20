16c2ce134c7af8f6186178d91522766d8d684cc6
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2025-05-20 23:25:47 +0530

    Updated Version

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using New_Student_Management_System.Main.Forms;
+
+namespace StudentManagementSystem
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new formMain());
+        }
+    }
+}
---
+++ StudentManagementSystem1.sql
@@ -0,0 +1,23 @@
+USE StudentManagementSystem;
+GO
+
+CREATE TABLE dbo.StudentTable
+(
+	UserID INT IDENTITY(1,1) PRIMARY KEY,
+	UserName VARCHAR(150) UNIQUE,
+	UserPass VARCHAR(150),
+	UserPho VARCHAR(15),
+	UserCNIC VARCHAR(15) UNIQUE,
+	UserDOB VARCHAR(10),
+	UserGender VARCHAR(10),
+	UserEmail VARCHAR(30),
+	UserRole VARCHAR(10),
+	UserAdd VARCHAR(150)
+);
+GO
+
+INSERT INTO dbo.StudentTable (UserName, UserPass, UserPho, UserCNIC, UserDOB, UserGender, UserEmail, UserRole, UserAdd)
+VALUES 
+('Student', '1234', '+91 9696799341', '222-222','31/10/2005', 'Male', 'sengar@gmail.com','Student','India'),
+('Faculty', '123', '+91 9696799341', '444-444','30/10/2008', 'Male', 'sengarajay@gmail.com','Faculty','India'),
+('Admin', '12345', '+91 9839153051', '333-333','12/07/2004', 'Male', 'sengarvishu@gmail.com','Admin','Russia');
